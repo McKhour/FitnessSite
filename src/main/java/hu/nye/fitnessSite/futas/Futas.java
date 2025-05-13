d452dd85f089ddc78a8902802ff878cba87fb9da
McKhour <arokricsi@gmail.com>
2025-05-13 22:09:35 +0200

    Hozzá adtam pár hiba kezelést

--- src/main/java/hu/nye/fitnessSite/futas/Futas.java
+++ src/main/java/hu/nye/fitnessSite/futas/Futas.java
@@ -1,13 +1,25 @@
 package hu.nye.fitnessSite.futas;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
+
 import java.time.LocalDateTime;
 
 public record Futas(
+        @NotEmpty
         Integer id,
+        @NotEmpty
         String futasCim,
         LocalDateTime futasKezd,
         LocalDateTime futasVeg,
+        @Positive
         Double kilometer,
         Helye helye
 ) {
+
+    public Futas{
+        if (!futasVeg.isAfter(futasKezd)){
+            throw new IllegalArgumentException("A Futás Vége nem előzheti meg a Futás Kezdetét!");
+        }
+    }
 }
