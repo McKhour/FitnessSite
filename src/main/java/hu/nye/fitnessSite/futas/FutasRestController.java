73f487fe9ea37fb1787515377737f7efeeded2a3
McKhour <arokricsi@gmail.com>
2025-05-16 12:25:35 +0200

    Sikerült megoldani hogy ha rányomok a linkre akkor a html-t nyissa meg, így a rest controller már az API-t fogja megnyitni

---
+++ src/main/java/hu/nye/fitnessSite/futas/FutasRestController.java
@@ -0,0 +1,64 @@
+package hu.nye.fitnessSite.futas;
+
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/futas")
+public class FutasRestController {
+
+    private final FutasRepository futasRepository;
+
+    public FutasRestController(FutasRepository futasRepository){
+        this.futasRepository = futasRepository;
+    }
+
+    @GetMapping("/api")
+    List<Futas> findAll(){
+        return futasRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    Futas findById(@PathVariable Integer id){
+        Optional<Futas> futas = futasRepository.findById(id);
+        if (futas.isEmpty()){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Futás nem található");
+        }
+        return futas.get();
+    }
+
+    //post
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("")
+    void create(@Valid @RequestBody Futas futas)
+    {
+        futasRepository.create(futas);
+    }
+
+    //put
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{id}")
+    void update(@Valid @RequestBody Futas futas, @PathVariable Integer id)
+    {
+        futasRepository.update(futas, id);
+    }
+
+    //delete
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    void delete(@PathVariable Integer id)
+    {
+        futasRepository.delete(id);
+
+    }
+
+    //@GetMapping("/futas")
+    //String futas(){
+    //    return "A futás helye";
+    //}
+}
