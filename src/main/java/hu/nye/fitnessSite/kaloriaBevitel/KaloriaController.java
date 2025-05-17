9f021d805216b57bb85f6e36848914bd2f9b6dd7
McKhour <arokricsi@gmail.com>
2025-05-17 22:34:18 +0200

    Táblázat be injektávla a kalóriához-hoz is

--- src/main/java/hu/nye/fitnessSite/kaloriaBevitel/KaloriaController.java
+++ src/main/java/hu/nye/fitnessSite/kaloriaBevitel/KaloriaController.java
@@ -1,15 +1,28 @@
 package hu.nye.fitnessSite.kaloriaBevitel;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/kaloriaBevitel")
 public class KaloriaController {
 
-    @GetMapping("")
-    public String kaloriaBevitel(){
+    private KaloriaBevitelRestController kaloriaRest;
+
+    @Autowired
+    public KaloriaController(KaloriaBevitelRestController kaloriaRest){
+        this.kaloriaRest = kaloriaRest;
+    }
+
+    @GetMapping("/list")
+    public String kaloriaList(Model model){
+        List<KaloriaBevitel> kaloria = kaloriaRest.findAll();
+        model.addAttribute("kaloria", kaloria);
         return "kaloriaBevitel/kaloriaBevitel";
     }
 }
