54ea7d33f2e57315394aef2280790fa674873538
McKhour <arokricsi@gmail.com>
2025-05-17 22:49:07 +0200

    Táblázat be injektávla a súlyemeléshez is

--- src/main/java/hu/nye/fitnessSite/sulyEmeles/SulyController.java
+++ src/main/java/hu/nye/fitnessSite/sulyEmeles/SulyController.java
@@ -1,14 +1,28 @@
 package hu.nye.fitnessSite.sulyEmeles;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/sulyEmeles")
 public class SulyController {
-    @GetMapping("")
-    public String sulyEmeles(){
+
+    private SulyEmelesRestController sulyRest;
+
+    @Autowired
+    public SulyController(SulyEmelesRestController sulyRest){
+        this.sulyRest = sulyRest;
+    }
+
+    @GetMapping("/list")
+    public String sulyList(Model model){
+        List<SulyEmeles> suly = sulyRest.findAll();
+        model.addAttribute("suly", suly);
         return "sulyEmeles/sulyEmeles";
     }
 }
