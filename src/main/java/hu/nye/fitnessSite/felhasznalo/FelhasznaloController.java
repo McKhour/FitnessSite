88414d588f81bf7e2122e4c9466cf716a3138771
McKhour <arokricsi@gmail.com>
2025-05-18 22:14:52 +0200

    Kisebb javítások, pl a RestController-ek eltávolítása

--- src/main/java/hu/nye/fitnessSite/felhasznalo/FelhasznaloController.java
+++ src/main/java/hu/nye/fitnessSite/felhasznalo/FelhasznaloController.java
@@ -1,6 +1,5 @@
 package hu.nye.fitnessSite.felhasznalo;
 
-import hu.nye.fitnessSite.felhasznalo.FelhasznaloRestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
